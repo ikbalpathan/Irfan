4c5c60f13844ea4ce9b58280d00a25b512d7ccc8
Pixster Macmini2 <Pixster-Macmini2@pixsters-mac-mini.local>
2016-08-30 17:50:09 +0530

    Initial Commit

---
+++ GitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitProject
+//
+//  Created by Pixster Macmini2 on 30/08/16.
+//  Copyright © 2016 Pixster Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
